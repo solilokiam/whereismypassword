d116ca053c59d931f6f8ae3d46b79a47455e4cef
Miquel Company Rodriguez <miquel.c@landbot.io>
2024-08-08 17:49:27 +0200

    feat: simple webauthn functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,45 +1,105 @@
-import Image from "next/image";
+"use client";
+
+import {
+  startAuthentication,
+  startRegistration,
+} from "@simplewebauthn/browser";
+import {
+  generateWebAuthnLoginOptions,
+  generateWebAuthnRegistrationOptions,
+  verifyWebAuthnLogin,
+  verifyWebAuthnRegistration,
+} from "../lib/auth/webauthn";
+import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <section className="h-screen">
-      <div className="container h-full px-6 py-24">
-        <div className="flex h-full flex-wrap items-center justify-center">
-          <div className="md:w-8/12 lg:ms-6 lg:w-5/12">
-            <form>
-              <div className="relative mb-6">
-                <input
-                  type="text"
-                  className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[2.15] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
-                  id="emailInput"
-                  placeholder="Email address"
-                />
-                <label
-                  htmlFor="emailInput"
-                  className="pointer-events-none absolute left-3 top-0 mb-0 max-w-[90%] origin-[0_0] truncate pt-[0.37rem] leading-[2.15] text-neutral-500 transition-all duration-200 ease-out peer-focus:-translate-y-[1.15rem] peer-focus:scale-[0.8] peer-focus:text-primary peer-data-[twe-input-state-active]:-translate-y-[1.15rem] peer-data-[twe-input-state-active]:scale-[0.8] motion-reduce:transition-none dark:text-neutral-400 dark:peer-focus:text-primary"
-                >
-                  Email address
-                </label>
-              </div>
-              <div className="mb-6 flex items-center justify-center">
-                <a
-                  href="#!"
-                  className="text-primary focus:outline-none dark:text-primary-400"
-                >
-                  Already a member?
-                </a>
-              </div>
+  const [errorText, setErrorText] = useState("");
 
-              <button
-                type="submit"
-                className="inline-block w-full rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong"
-              >
-                Sign up
-              </button>
-            </form>
-          </div>
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const formData = new FormData(event.currentTarget);
+    const email = formData.get("email")?.toString();
+    const type = formData.get("type")?.toString();
+
+    if (!email) {
+      return;
+    }
+
+    if (type === "register") {
+      const response = await generateWebAuthnRegistrationOptions(email);
+
+      if (!response.success || !response.data) {
+        setErrorText(response.message ?? "Something went wrong!");
+        return;
+      }
+
+      const localResponse = await startRegistration(response.data);
+      const verifyResponse = await verifyWebAuthnRegistration(localResponse);
+
+      if (!verifyResponse.success) {
+        setErrorText(verifyResponse.message ?? "Something went wrong!");
+        return;
+      }
+
+      alert("Registration successful!");
+    } else {
+      const response = await generateWebAuthnLoginOptions(email);
+
+      if (!response.success || !response.data) {
+        setErrorText(response.message ?? "Something went wrong!");
+        return;
+      }
+
+      const localResponse = await startAuthentication(response.data);
+      const verifyResponse = await verifyWebAuthnLogin(localResponse);
+
+      if (!verifyResponse.success) {
+        setErrorText(verifyResponse.message ?? "Something went wrong!");
+        return;
+      }
+
+      setErrorText("Login successful!");
+    }
+  };
+
+  return (
+    <main className={styles.main}>
+      {errorText ?? <div>{errorText}</div>}
+      <form onSubmit={handleSubmit} className={styles.formContainer}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Email"
+          className={styles.inputField}
+        />
+
+        <div className={styles.radioGroup}>
+          <label className={styles.label}>
+            <input
+              type="radio"
+              name="type"
+              value="register"
+              defaultChecked
+              className={styles.radioInput}
+            />
+            Register
+          </label>
+
+          <label className={styles.label}>
+            <input
+              type="radio"
+              name="type"
+              value="login"
+              className={styles.radioInput}
+            />
+            Login
+          </label>
         </div>
-      </div>
-    </section>
+
+        <input type="submit" value="Submit" className={styles.submitButton} />
+      </form>
+    </main>
   );
 }
