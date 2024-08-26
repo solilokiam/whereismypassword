6ea748e438d359921611312f7af535ca60a2e24e
Miquel Company Rodriguez <miquel.c@landbot.io>
2024-08-26 14:52:03 +0200

    change register word

--- app/page.tsx
+++ app/page.tsx
@@ -87,7 +87,7 @@ export default async function Home() {
                   href="/signup"
                   className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
-                  Get started
+                  Register
                 </Link>
                 <Link
                   href="/signin"
